3656b93322c89dcf5234557f8777e4dd3bb12f3c
Himanshu Sajwan <57579061+HimanshuSajwan911@users.noreply.github.com>
2021-07-17 10:43:12 +0530

    linear_search.java
    Given an array of nonnegative integers, design a linear algorithm and implement it using a program to find whether a given key element is present in the array or not. Also, find the total number of comparisons for each input case.

--- Week0/linear_search.java
+++ Week0/linear_search.java
@@ -1,1 +1,52 @@
+package DAA;
+
+import java.util.*;
+
+public class linear_search {
+
+	public static boolean search(int[] arr, int k){
+		int i;
+		boolean res = false;;
+		for(i = 0; i < arr.length; i++){
+			if(arr[i] == k){
+				res = true;
+				break;
+			}
+		}
+		System.out.println("Total number of comparisons = " + (i + 1));
+		return res;
+	}
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input number of test cases: ");
+		int T = sc.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			System.out.print("\nInput number of elements: ");
+			int N = sc.nextInt();
+			
+			System.out.println("Input elements:");
+			int[] arr = new int[N];
+			
+			for (int j = 0; j < N; j++) {
+				arr[j] = sc.nextInt();
+			}
+			
+			System.out.print("Input key (K): ");
+			int K = sc.nextInt();
+			
+			if(search(arr, K)){
+				System.out.println("Yes! key(" + K + ") is present in array.");
+			}
+			else{
+				System.out.println("No! key(" + K + ") is not present in array.");
+			}
+
+		}
+
+	}
+
+}
 
